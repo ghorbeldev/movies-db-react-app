9b326365bb2fbefdce9573b6c5256a17d2fc9856
ghorbeldev <adem.ghorbel9@gmail.com>
2022-02-04 19:15:35 +0100

    ''

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,5 +1,5 @@
 const apiConfig = {
-	baseUrl: 'https:/api.themoviedb.org/3/',
+	baseUrl: 'https://api.themoviedb.org/3/',
 	apiKey: '8cd734cbad818ac364bbd1710510ae69',
 	originalImage: imgPath => `https://image.tmdb.org/t/p/original/${imgPath}`,
 	w500Image: imgPath => `https://image.tmdb.org/t/p/w500/${imgPath}`,
